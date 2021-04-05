0e67978c2debc444e32a3555c36af1890308c702
jjnacher <60861603+jjnacher@users.noreply.github.com>
2021-04-05 12:58:10 +0100

    poli

--- src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
+++ src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClubesportiuApplication {
 
 	public static void main(String[] args) {
-		System.out.println("---------Hola---------");
+		System.out.println("--segunda pr----");
 		//SpringApplication.run(ClubesportiuApplication.class, args);
 	}
 
