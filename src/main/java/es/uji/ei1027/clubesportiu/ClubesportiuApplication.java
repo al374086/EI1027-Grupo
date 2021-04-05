b27e86562633136a85e965f676f2768f2ab9a661
Alex <Alex@192.168.1.129>
2021-04-05 14:16:54 +0200

    Actualizado cuarta prueba

--- src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
+++ src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClubesportiuApplication {
 
 	public static void main(String[] args) {
-		System.out.println("--tercera pr----");
+		System.out.println("--cuarta pr----");
 		//SpringApplication.run(ClubesportiuApplication.class, args);
 	}
 
