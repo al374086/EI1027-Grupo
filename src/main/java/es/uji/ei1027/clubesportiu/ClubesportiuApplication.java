33b2d881eae20c964607c3abe7a5d892d4bcc3ea
pelans <47571151+pelans@users.noreply.github.com>
2021-04-05 13:56:37 +0200

    update2 test
    test

--- src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
+++ src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClubesportiuApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Tercera Prueba Samir Carlos 13:53");
+		System.out.println("---------Hola---------");
 		//SpringApplication.run(ClubesportiuApplication.class, args);
 	}
 
