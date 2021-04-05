2395486d4393517597140331240728e3e15374ee
SAM <al375882@uji.es>
2021-04-05 13:26:54 +0200

    samir

--- src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
+++ src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClubesportiuApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Tercera Prueba");
+		System.out.println("Tercera Prueba Samir");
 		//SpringApplication.run(ClubesportiuApplication.class, args);
 	}
 
