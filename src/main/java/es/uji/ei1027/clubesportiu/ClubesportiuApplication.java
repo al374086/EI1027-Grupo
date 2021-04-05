5d2d1f9b68cf3b2cf4c34eb0ee168dad8bae6a54
jjnacher <60861603+jjnacher@users.noreply.github.com>
2021-04-05 13:13:05 +0100

    otra prueba

--- src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
+++ src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClubesportiuApplication {
 
 	public static void main(String[] args) {
-		System.out.println("--segunda pr----");
+		System.out.println("--tercera pr----");
 		//SpringApplication.run(ClubesportiuApplication.class, args);
 	}
 
