5a04caf4b5dfc555765a9baf053750afe67a20d8
pelans <47571151+pelans@users.noreply.github.com>
2021-04-05 12:28:35 +0200

    ClubEsporti
    Base

---
+++ ProyectoGrupo/src/main/java/es/uji/ei1027/clubesportiu/ClubesportiuApplication.java
@@ -0,0 +1,14 @@
+package es.uji.ei1027.clubesportiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubesportiuApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Prueba");
+		//SpringApplication.run(ClubesportiuApplication.class, args);
+	}
+
+}
