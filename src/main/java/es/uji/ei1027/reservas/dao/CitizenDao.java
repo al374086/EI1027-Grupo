ef4969353e85e95391cc204f8f8f84eb0d36514e
pelans <47571151+pelans@users.noreply.github.com>
2021-04-13 09:53:44 +0200

    Citizen
    + Dao, RowMapper, Citizen

---
+++ src/main/java/es/uji/ei1027/reservas/dao/CitizenDao.java
@@ -0,0 +1,87 @@
+package es.uji.ei1027.reservas.dao;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import es.uji.ei1027.reservas.modelo.Citizen;
+import es.uji.ei1027.reservas.modelo.TemporalService;
+
+@Repository
+public class CitizenDao {
+	
+	
+   private JdbcTemplate jdbcTemplate;
+
+   @Autowired
+   public void setDataSource(DataSource dataSource) {
+       jdbcTemplate = new JdbcTemplate(dataSource);
+   }
+
+   /* Afegeix un temporalService a la base de dades */
+   public void addTemporalService(Citizen citizen) {
+       jdbcTemplate.update("INSERT INTO citizen VALUES(?, ?, ?, ?, ?, ?, ?)",
+    		   citizen.getDni(), citizen.getName(), citizen.getAddress(), citizen.getTown(), citizen.getCountry(),
+    		   citizen.getCp(), citizen.getPin());
+   }
+
+   /* Esborra un temporalService de la base de dades */
+   public void deleteTemporalService(int dni) {
+       jdbcTemplate.update("DELETE from citizen where dni=?",
+    		   dni);
+   }
+   public void deleteTemporalservice(Citizen citizen) {
+       jdbcTemplate.update("DELETE from citizen where dni=?",
+    		   citizen.getDni());
+   }
+
+   /* Actualitza els atributs del citizen
+      (excepte el dni  que és la clau primària) */
+   public void updatePlan(Citizen citizen) {
+       jdbcTemplate.update("UPDATE citizen SET name=?, address=?, town=?, country=?, cp=?, pin=? where dni=?",
+    		   citizen.getName(), citizen.getAddress(), citizen.getTown(), citizen.getCountry(), citizen.getCp(),
+    		   citizen.getPin(), citizen.getDni());
+   }
+
+   /* Obté el Citizen amb el dni donat. Torna null si no existeix. */
+   public Citizen getTemporalservice (int dni) {
+       try {
+           return jdbcTemplate.queryForObject("SELECT * from citizen WHERE dni=?",
+                   new CitizenRowMapper(), dni);
+       }
+       catch(EmptyResultDataAccessException e) {
+           return null;
+       }
+   }
+
+   /* Obté tots els plans. Torna una llista buida si no n'hi ha cap. */
+   public List<Citizen> getCitizens() {
+       try {
+           return jdbcTemplate.query("SELECT * from citizen",
+                   new CitizenRowMapper());
+       } catch (EmptyResultDataAccessException e) {
+           return new ArrayList<Citizen>();
+       }
+   }
+   
+   
+   public List<Citizen> getCitizens(String dni) {
+	   try {
+	     return this.jdbcTemplate.query(
+	           "SELECT * FROM citizen WHERE dni=?",
+	           new Object[] {dni}, new CitizenRowMapper());
+	  }
+	  catch(EmptyResultDataAccessException e) {
+	      return new ArrayList<Citizen>();
+	  }
+	}
+
+}
