02e872745ced94a689be97ff035f2cf98503df58
pelans <47571151+pelans@users.noreply.github.com>
2021-06-12 20:36:01 +0200

    Un par de cosas

--- src/main/java/es/uji/ei1027/reservas/controller/RegistroController.java
+++ src/main/java/es/uji/ei1027/reservas/controller/RegistroController.java
@@ -1,5 +1,7 @@
 package es.uji.ei1027.reservas.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import es.uji.ei1027.reservas.dao.CitizenDao;
 import es.uji.ei1027.reservas.modelo.Citizen;
+import es.uji.ei1027.reservas.modelo.Usuario;
 
 @Controller
 @RequestMapping("/user")
@@ -25,15 +28,24 @@ public class RegistroController {
 	}
 
 	@RequestMapping(value = "/addRegistro")
-	public String addCitizen(Model model) {
+	public String addCitizen(HttpSession session, Model model) {
+		if (session.getAttribute("user") != null) 
+	       { 
+	          return "redirect:/user/login";
+	       }
 		model.addAttribute("citizen", new Citizen());
 		return "user/addRegistro";
 	}
 
 	@RequestMapping(value = "/addRegistro", method = RequestMethod.POST)
-	public String processAddSubmit(@ModelAttribute("citizen") Citizen citizen, BindingResult bindingResult) {
+	public String processAddSubmit(HttpSession session, @ModelAttribute("citizen") Citizen citizen, BindingResult bindingResult) {
 		if (bindingResult.hasErrors())
 			return "user/addRegistro";
+		
+		if (session.getAttribute("user") != null) 
+	       { 
+	          return "redicrect:/user/login";
+	       }
 		
 		citizenDao.addCitizen(citizen);
 		
